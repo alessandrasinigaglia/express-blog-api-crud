0729a850e3f5f7acaaf8142aecba0179b597af66
alessandrasinigaglia <alessandra.sinigaglia06@gmail.com>
2025-01-27 11:42:28 +0100

    feat: add controller

---
+++ controllers/postsController.js
@@ -0,0 +1,28 @@
+const postsData = require('../data/postsData')
+
+//index
+const index = (req, res) => {
+    res.send('Lista dei post')
+}
+
+//show
+const show = (req, res)  => {
+    res.send('Dettagli del post')
+}
+
+//create
+const create = (req, res) => {
+    res.send('Creazione nuovo post')
+}
+
+//update
+const update = (req, res) => {
+    res.send(`Modifica integrale del post: ${req.params.id}`)
+}
+
+//delete
+const destroy = (req, res) => {
+    res.send(`Eliminazione del post: ${req.params.id}`)
+}
+
+module.exports = { index, show, create, update, destroy }
