a44af7d6349b8b2df3e7982d0d45259b0b2051a5
alessandrasinigaglia <alessandra.sinigaglia06@gmail.com>
2025-01-30 10:03:01 +0100

    middleware

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const postsRouter = require('./routers/posts')
+const express = require('express');
+const app = express();
+const port = 3000;
+const checktime = require("./middleware/checkTime");
+const postsRouter = require('./routers/posts');
+const errorHandler = require("./middleware/errorHandler");
+
+app.use(checktime);
 
 app.use(express.static('public'))
 app.use(express.json());
 
+
 app.get('/', (req, res) => {
   res.send('Server del mio blog')
-})
+});
 
-app.use('/posts', postsRouter)
+app.use('/posts', postsRouter);
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
